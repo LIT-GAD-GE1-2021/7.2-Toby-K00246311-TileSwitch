9ed53a8aa41ea18e257b7fa5d4d7c71fbc16431e
Toby.Taylor <toby.taylor20@yahoo.com>
2021-02-25 09:56:32 +0000

    Finished CBA

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+    public GameObject redWall;
+    public GameObject greenWall;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void SwitchTileMap()
+    {
+        redWall.SetActive(!redWall.activeSelf);
+        greenWall.SetActive(!greenWall.activeSelf);
+    }
+    
+}
